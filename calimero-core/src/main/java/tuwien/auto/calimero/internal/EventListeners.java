96480460a2b55684c6840e87a332bb3a579fb862
yangkai <215496281@qq.com>
2021-08-19 19:24:04 +0800

    去除一个log

--- calimero-core/src/main/java/tuwien/auto/calimero/internal/EventListeners.java
+++ calimero-core/src/main/java/tuwien/auto/calimero/internal/EventListeners.java
@@ -124,7 +124,6 @@ public class EventListeners<T>
 		final List<T> list = listeners();
 		for (final T l : list) {
 			try {
-				logger.debug("eventListeners fire");
 				c.accept(l);
 			}
 			catch (final RuntimeException rte) {
